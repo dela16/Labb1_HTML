aa9ce8a8ed9f2b22148d23dc5c35674854ce8bdf
dela16 <Denice.94@hotmail.com>
2022-02-22 23:20:56 +0100

    nu kanske allt är på plats(inte allt)

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,47 +1,44 @@
-const courses = `[{"Kursnummer": "1",
-    "Kurstitel": "Vad är internet?", 
-    "Kursbeskrivning": "lorem ipsum", 
-    "Längd": "10 veckor."},
-
- {"Kursnummer": "2",
-    "Kurstitel": "C#, Java, C++ och mycket mer", 
-    "Kursbeskrivning": "lorem ipsum", 
-    "Längd": "40 veckor."},
-
- {"Kursnummer": "3", 
-    "Kurstitel": "AI - Prata med robotar.", 
-    "Kursbeskrivning": "lorem ipsum", 
-    "Längd": "10 veckor."},
-
- {"Kursnummer": "4", 
-    "Kurstitel": "Cloud - Mer än fluffiga moln.", 
-    "Kursbeskrivning": "lorem ipsum", 
-   "Längd": "25 veckor."},
-
- {"Kursnummer": "5", 
-    "Kurstitel": "Office 365, grundkurs.", 
-    "Kursbeskrivning":"lorem ipsum", 
-    "Längd": "5 veckor."},
-
- {"Kursnummer": "6", 
-    "Kurstitel": "Paint - Din bästa vän.", 
-    "Kursbeskrivning": "lorem ipsum", 
-    "Längd": "5 veckor."},
-
-  {"Kursnummer": "7", 
-    "Kurstitel": "Office 365, Advanced.", 
-    "Kursbeskrivning": "lorem ipsum", 
-    "Längd": "10 veckor."},
-
-   {"Kursnummer": "8", 
-    "Kurstitel": "Paint - Din bästa vän, Advanced", 
-    "Kursbeskrivning": "lorem ipsum" , 
-    "Längd": "10 veckor."}]`
-
+const courses = `[{"Kursnummer":"1","Kurstitel":"Vad är internet?","Kursbeskrivning":"lorem ipsum","Längd":"10 veckor."}, {"Kursnummer":"2","Kurstitel":"C#, Java, C++ och mycket mer","Kursbeskrivning":"lorem ipsum","Längd":"40 veckor."},{"Kursnummer":"3","Kurstitel":"AI - Prata med robotar.","Kursbeskrivning":"lorem ipsum","Längd":"10 veckor."},{"Kursnummer":"4","Kurstitel":"Cloud - Mer än fluffiga moln.","Kursbeskrivning":"lorem ipsum","Längd":"25 veckor."},{"Kursnummer":"5","Kurstitel":"Office 365, grundkurs.","Kursbeskrivning":"lorem ipsum","Längd":"5 veckor."},{"Kursnummer":"6","Kurstitel":"Paint - Din bästa vän.","Kursbeskrivning":"lorem ipsum","Längd":"5 veckor."},{"Kursnummer":"7","Kurstitel":"Office 365, Advanced.","Kursbeskrivning":"lorem ipsum","Längd":"10 veckor."},{"Kursnummer":"8","Kurstitel":"Paint - Din bästa vän, Advanced","Kursbeskrivning":"lorem ipsum","Längd":"10 veckor."}]`
 const courseList = JSON.parse(courses);
 
-
 const saveBtn = document.getElementById("saveBtn");
+
+const cart = []; 
+
+
+function updateCart() {
+    const cart = document.getElementById("coursesInCart"); 
+
+   if (coursesInCart.length <= 0) {
+      const extraCartDiv = document.createElement("div");
+      extraCartDiv.innerText = "Cart is empty."
+      cart.appendChild(extraCartDiv);
+   };
+
+}
+
+function addCourseToCart(courseNumber) {
+   let courseWeWantToAdd = ""
+    for (let i = 0; i < courseList.length; i++) {
+      const course = courseList[i];
+       if (course.Kursnummer == courseNumber) {
+         courseWeWantToAdd = course
+         let wceCourses = `<li>Kursnummer: ${course.Kursnummer}, Kurstitel: ${course.Kurstitel}, Kursbeskrivning: ${course.Kursbeskrivning}, Längd: ${course.Längd}</li>`;
+         document.getElementById("coursesInCart").innerHTML += wceCourses;
+         break
+       }
+    };
+   
+   if (cart.includes(courseWeWantToAdd)) {
+      console.log("Course is already in cart.");
+   } else {
+      cart.push(courseWeWantToAdd);
+      document.getElementById("emptyCartText").innerHTML = "";
+      console.log(cart);
+      updateCart()
+   }
+     
+}
 
 saveBtn.addEventListener("click", () => {//Här sparar och lägger vi till den nya kursen i listan på admin sidan. 
 
@@ -56,97 +53,25 @@ saveBtn.addEventListener("click", () => {//Här sparar och lägger vi till den n
    kurs.Kursbeskrivning = cDescription;
    kurs.Längd = cLength;
    //Lägga till bild via url i kursform?
+   adminModalContainer.classList.remove("show")
    courseList.push(kurs);
    getCourses(); 
-   adminModalContainer.classList.remove("show");
-   //Här kan du skriva in kod för att tömma rutorna. Göra det till en funktion? 
    printAllCourseCards(); 
 });
 
-
-//Vad som krävs för att kunna skapa ett kort till presentationssidan. Kanske inte jätteviktigt. Funkar inte just nu. 
-function createCourseCard(cNumber, cTitel,cDescription,cLength) {
-   if (cNumber == "") {
-      window.alert("Add a course Number");
-      return; 
-      
-   }
-
-   let existingCourse = false; 
-   const newCourse = new kurs(cNumber, cTitel, cDescription, cLength);
-   courses.forEach((course) => {
-      if (course.cNumber == newCourse.cNumber) {
-         existingCourse = true; 
-         return; 
-      }
-   });
-
-   if (existingCourse) {
-      window.alert("Must be a unique course Number.");
-      return; 
-   }
-   courseList.push(newCourse);
-   printAllCourseCards(); 
-
+function getCourses() {
+    console.log("Tryckte på Admin-knappen")
+    let kurser = document.getElementById("existingCourses");
+    kurser.innerHTML = ""; 
+    
+   for (let i = 0; i < courseList.length; i++) {
+      const course = courseList[i];
+      let wceCourses = `<li>Kursnummer: ${course.Kursnummer}, Kurstitel: ${course.Kurstitel}, Kursbeskrivning: ${course.Kursbeskrivning}, Längd: ${course.Längd}</li>`;
+      kurser.innerHTML += wceCourses;
+    };
+    
+    adminDiv.classList.add("show");
 }
-
-
-
-const cart = []; 
-
-function addCourseToCart(courseNumber) {
-   console.log(cart);
-
-   let addingCourseToCart = courses.find((element) => element.Kursnummer === courseNumber);
-   const kundvagn = document.getElementById("coursesInCart"); 
-   kundvagn.innerHTML = `<li>${addingCourseToCart.Kurstitel} ${addingCourseToCart.Kursnummer}</li>`;
-   //Något med detta för att få kurserna till kundvagnen. Något med knappen. 
-
-
-
-   
-   if (cart.includes(addingCourseToCart)) {
-      console.log("Course is already in cart.");
-      return;
-   } else {
-      cart.push(addingCourseToCart);
-
-
-      updateCart();
-
-
-      //Jag tänker mig att den här ska vara med på något vis. 
-      cart.forEach((cartItem) => {
-         const liDiv = document.createElement("div");
-         liDiv.classList.add("li");
-
-         const headLine = document.createElement("h1");
-         headLine.innerText = courseItem.Kurstitel;
-
-
-         liDiv.appendChild(headLine);
-
-         cartItem.appendChild(liDiv);
-
-         console.log(`${addingCourseToCart.cTitel} added`);
-      
-      })
-
-   }
-     
-   
-}
-
-function removeCourseFromCart(cartItem) {
-   const cartItemToRemove = cart.find((el) => el.cNumber === cartItem);
-
-   console.log(cart.indexOf(cartItemToRemove));
-   cart.splice(cart.indexOf(cartItemToRemove), 1);
-   
-   updateCart();
-   
-}
-
 
 function printAllCourseCards() {
   
@@ -176,95 +101,72 @@ function printAllCourseCards() {
       mainDiv.appendChild(cardDiv);
 
    })
+
+function removeCourseFromCart(cartItem) {
+   const cartItemToRemove = cart.find((el) => el.cNumber === cartItem);
+
+   console.log(cart.indexOf(cartItemToRemove));
+   cart.splice(cart.indexOf(cartItemToRemove), 1);
    
+   updateCart();
+   
+}
+
 
 }
 
 
-printAllCourseCards(); 
+const addNewCourseBtn = document.getElementById("addNewCourseBtn");
+const adminModalContainer = document.getElementById("adminModalContainer");
+const closeAdminModalBtn = document.getElementById("closeAdminModalBtn"); 
+const openCartModal = document.getElementById("openCartModal");
+const kundvagnDiv = document.getElementById("kundvagn");
+const closeCartBtn = document.getElementById("closeCartBtn"); 
+const buyBtn = document.getElementById("buyBtn");
+const cartPayedModal = document.getElementById("cartPayedModal");
+const closeBtn = document.getElementById("closeBtn");
+const openAdminModal = document.getElementById("openAdminModal");
+const adminDiv = document.getElementById("admin");
+const closeAdminDiv = document.getElementById("closeAdminBtn");
 
-
-
-function updateCart() {//Detta är säkert fel. Kolla din addCourseToCart metod. rad 97. 
-    const cart = document.getElementById("coursesInCart"); 
-   cart.innerHTML = ""; 
-
-   if (coursesInCart.length <= 0) {
-      const extraCartDiv = document.createElement("div");
-      extraCartDiv.innerText = "Cart is empty."
-      cart.appendChild(extraCartDiv);
-   };
-
+function myBurgerMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";         
+    } else {
+        x.style.display = "block"; 
+    }
 }
 
+openAdminModal.addEventListener("click", () => {
+    getCourses(); 
+});
 
+closeAdminDiv.addEventListener("click", () => {
+    adminDiv.classList.remove("show");
+});
 
+addNewCourseBtn.addEventListener("click", () => {
+    adminModalContainer.classList.add("show");
+});
+closeAdminModalBtn.addEventListener("click", () => {
+    adminModalContainer.classList.remove("show");
+});
 
+openCartModal.addEventListener("click", () => {
+    kundvagnDiv.classList.add("show");
+});
 
+closeCartBtn.addEventListener("click", () => {
+    kundvagnDiv.classList.remove("show");
+});
 
+buyBtn.addEventListener("click", () => {
+    cartPayedModal.classList.add("show");
+});
 
-
-
-// 
-
-// function 
- 
-// const PaintAdvanced = document.getElementById("paint-Advanced");
-
-// PaintAdvanced.addEventListener("click", () => {
-//    addToCart(); 
-// });
-
-// function addToCart() {
-//    kurs.Kursnummer; 
-//    kurs.Kurstitel; 
-
-//    coursesInCart.push(kurs);
-// }
-
-// function addToCart() {
-//    const item = document.getElementById(coursesInCart);
-
-//    //all elements to be added
-
-
-//    //push elements into HTML
-//    item.appendChild(ele);
-//    ele.appendChild(pic);
-//    ele.appendChild(price);
-//    ele.appendChild(desc);
-//    ele.appendChild(add);
-//    ele.appendChild(typeBox);
-
-//    //edit pushed elements info from array.
-//    pic.innerHTML = courses[i][0];
-//    price.innerHTML = "$" + courses[i][1];
-//    desc.innerHTML = courses[i][2];
-//    add.innerHTML = "add"; 
-//    typeBox.typ = "number"; 
-
-
-// }
-
-// if (buyCourseBtn.addEventListener("click", () => {
-//    kurs.Kursnummer = "1";
-//    kurs.Kurstitel = "BuyInternet";
-//    kurs.Kursbeskrivning = "lorem Ipsum";
-//    kurs.Längd = "10 veckor.";
-// })); {
-
-//     for (let i = 0; i < cart.length; i++) {
-//             const cartItem = cart[i];
-//             let courseItem = `<li>Kursnummer: ${cartItem.Kursnummer}, Kurstitel: ${cartItem.Kurstitel}, Kursbeskrivning: ${cartItem.Kursbeskrivning}, Längd: ${cartItem.Längd}</li>`;
-//             document.getElementById("coursesInCart").innerHTML += courseItem;
-//         };
-
-//    cart.push("hej"); 
-//  } 
-
-//        //alternativt!
-
-// const buyCourseBtn = document.getElementById("buyBtn");//OBS! Detta ID finns inte i nuläget!
-
+closeBtn.addEventListener("click", () => {
+    cartPayedModal.classList.remove("show");
+});
 
     
