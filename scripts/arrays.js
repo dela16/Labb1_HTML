1c8bb7590cb26d1fb60072677d98ff0da3673322
dela16 <Denice.94@hotmail.com>
2022-02-25 15:21:14 +0100

    burgarmeny funkar i mobilläge

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -124,10 +124,10 @@ const closeAdminDiv = document.getElementById("closeAdminBtn");
 
 function myBurgerMenu() {
   var x = document.getElementById("myLinks");
-  if (x.style.display == "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display == "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
 
